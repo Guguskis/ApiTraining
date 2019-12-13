0cffcfb601de4a1102f9495b99e4e5ea82ce5b0b
Guguskis <matasliutikas@gmail.com>
2019-12-13 12:57:21 +0200

    mock endpoint is reset after each test

--- dropbox/src/test/java/lt/liutikas/dropbox/route/validation/ValidationRouteTest.java
+++ dropbox/src/test/java/lt/liutikas/dropbox/route/validation/ValidationRouteTest.java
@@ -6,6 +6,7 @@ import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -17,20 +18,19 @@ import java.util.List;
 @ContextConfiguration(classes = WebConfig.class)
 class ValidationRouteTest {
 
-    //Todo
-//    @After
-//    public void tearDown() throws Exception {
-//        mockEndpoint.reset();
-//    }
-
     @EndpointInject("mock:result")
     private MockEndpoint mock;
 
     @Produce("direct:validation")
     private ProducerTemplate template;
 
+    @AfterEach
+    public void tearDown() throws Exception {
+        mock.reset();
+    }
+
     @Test
-    void route_CorrectData_ReturnsTrue() throws InterruptedException {
+    public void route_CorrectData_ReturnsTrue() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael", "1", "1"));
@@ -44,7 +44,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_NoData_ReturnsFalse() throws InterruptedException {
+    public void route_NoData_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
 
         mock.expectedHeaderReceived("valid", false);
@@ -54,7 +54,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_OnlyProperties_ReturnsFalse() throws InterruptedException {
+    public void route_OnlyProperties_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
 
@@ -65,7 +65,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_NoProperties_ReturnsFalse() throws InterruptedException {
+    public void route_NoProperties_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("Michael", "1", "1"));
         body.add(Arrays.asList("Jan", "3", "3"));
@@ -77,7 +77,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_DuplicateProperty_ReturnsFalse() throws InterruptedException {
+    public void route_DuplicateProperty_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael", "Michael", "1", "1"));
@@ -89,7 +89,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_MissingRequiredProperty_ReturnsFalse() throws InterruptedException {
+    public void route_MissingRequiredProperty_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId"));
         body.add(Arrays.asList("Michael", "1"));
@@ -101,7 +101,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_RowHasTooManyValues_ReturnsFalse() throws InterruptedException {
+    public void route_RowHasTooManyValues_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael", "1", "1", "extra value"));
@@ -113,7 +113,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_RowHasMissingValues_ReturnsFalse() throws InterruptedException {
+    public void route_RowHasMissingValues_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael", "1"));
@@ -125,7 +125,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_ValueIsStringWhereLongExpected_ReturnsFalse() throws InterruptedException {
+    public void route_ValueIsStringWhereLongExpected_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael", "1", "string value"));
@@ -137,7 +137,7 @@ class ValidationRouteTest {
     }
 
     @Test
-    void route_ValueContainsNumbersWhereOnlyLettersExpected_ReturnsFalse() throws InterruptedException {
+    public void route_ValueContainsNumbersWhereOnlyLettersExpected_ReturnsFalse() throws InterruptedException {
         List<List<String>> body = new ArrayList<>();
         body.add(Arrays.asList("name", "officialId", "languageId"));
         body.add(Arrays.asList("Michael3123", "1", "1"));
