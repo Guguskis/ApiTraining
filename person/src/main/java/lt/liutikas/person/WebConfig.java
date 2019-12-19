e7e868bfd2b2ca2e3b0ade7205716cbd76812f65
Guguskis <matasliutikas@gmail.com>
2019-12-19 10:32:09 +0200

    removed unnecessary ComponentScan annotations

--- person/src/main/java/lt/liutikas/person/WebConfig.java
+++ person/src/main/java/lt/liutikas/person/WebConfig.java
@@ -6,13 +6,11 @@ import org.apache.camel.impl.engine.DefaultProducerTemplate;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @EntityScan("lt.liutikas.model")
-@ComponentScan("lt.liutikas")
 public class WebConfig {
     @Bean
     public ProducerTemplate producerTemplate() {
